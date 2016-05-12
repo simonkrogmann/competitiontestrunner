9c4e306d0aaa3d138d656a4669942ce7ee310f3f
Simon Krogmann <simon.krogmann@student.hpi.de>
2016-05-12 14:43:13 +0200

    Improve python template

--- filecreator/resource/generate.py
+++ filecreator/resource/generate.py
@@ -1,2 +1,3 @@
 # File for generating large test cases
 # Use print to output test cases
+print(0)
--- filecreator/resource/template.py
+++ filecreator/resource/template.py
@@ -6,20 +6,26 @@ def problem(case):
     return case
 
 
+def nextline(input_file):
+    data = ""
+    while not data:
+        data = input_file.readline()
+    return data[:-1]
+
+
 def main():
+    result = ""
+    with sys.stdin if len(sys.argv) == 1 else open(sys.argv[1], 'r') as infile:
+        number = int(nextline(infile))
+        for run in range(number):
+            case = nextline(infile)
+            result += 'Case #{}: {}\n'.format(1 + run, problem(case))
+
     if len(sys.argv) == 1:
-        pass
-    name = sys.argv[1]
-    with open(name, 'r') as input_file:
-        content = input_file.read()
-    lines = content.splitlines()
-    num = lines[0]
-    result = ''
-    for run in range(int(num)):
-        case = lines[1 + run]
-        result += 'Case #{}: {}\n'.format(1 + run, problem(case))
-    with open(name.replace('in', 'sol'), 'w') as result_file:
-        result_file.write(result)
+        print(result, end='')
+    else:
+        with open(sys.argv[1].replace('in', 'sol'), 'w') as result_file:
+            result_file.write(result)
 
 if __name__ == '__main__':
     main()
